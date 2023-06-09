fc5e421a2fac659d7612c54d148b753c608e58aa
mahmoud shady <mahmoudshady20>
2023-06-09 04:53:33 +0300

    edit in sebha screen

--- lib/sharedpreferences/sharedpreferences.dart
+++ lib/sharedpreferences/sharedpreferences.dart
@@ -19,4 +19,46 @@ import 'package:shared_preferences/shared_preferences.dart';
     return prefs.getString('lan') ?? 'ar';
   }
 
+  static Future<void> setCounter1(int counter) async {
+   await prefs.setInt('counter1', counter);
+  }
+
+  static int getCounter1(){
+   return prefs.getInt('counter1') ?? 0;
+  }
+  static Future<void> setCounter2(int counter) async {
+   await prefs.setInt('counter2', counter);
+  }
+
+  static int getCounter2(){
+   return prefs.getInt('counter2') ?? 0;
+  }
+  static Future<void> setCounter3(int counter) async {
+   await prefs.setInt('counter3', counter);
+  }
+
+  static int getCounter3(){
+   return prefs.getInt('counter3') ?? 0;
+  }
+  static Future<void> setCounter4(int counter) async {
+   await prefs.setInt('counter4', counter);
+  }
+
+  static int getCounter4(){
+   return prefs.getInt('counter4') ?? 0;
+  }
+  static Future<void> setCounter5(int counter) async {
+   await prefs.setInt('counter5', counter);
+  }
+
+  static int getCounter5(){
+   return prefs.getInt('counter5') ?? 0;
+  }
+  static Future<void> setCounter6(int counter) async {
+   await prefs.setInt('counter6', counter);
+  }
+
+  static int getCounter6(){
+   return prefs.getInt('counter6') ?? 0;
+  }
 }
