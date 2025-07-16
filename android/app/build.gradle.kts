a12c87f3dd19dbdf5701e80838fc01226a051e5c
mahmoudshady20 <mahmoud4ady@gmail.com>
2025-07-16 03:22:32 +0300

    1- finish android release

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,11 +1,9 @@
 import java.util.Properties
 import java.io.FileInputStream
 
-
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
@@ -18,7 +16,7 @@ if (keystorePropertiesFile.exists()) {
 android {
     namespace = "com.mahmoudshady.islamirevision"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973" // ✅ تم التعديل هنا
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -30,15 +28,13 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.mahmoudshady.islamirevision"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        applicationId = "com.mahmoudshady.islamiApp"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        versionCode = 4 // ✅ تم التعديل هنا
+        versionName = "1.3.0" // ✅ تم التعديل هنا
     }
+
     signingConfigs {
         create("release") {
             keyAlias = keystoreProperties["keyAlias"] as String
@@ -47,10 +43,9 @@ android {
             storePassword = keystoreProperties["storePassword"] as String
         }
     }
+
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("release")
         }
     }
